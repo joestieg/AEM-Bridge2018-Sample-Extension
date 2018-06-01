aa796d823e31c5ad0ca8e395b1aeed96f6e4a9c2
joestieg <39833188+joestieg@users.noreply.github.com>
2018-06-01 14:49:06 -0400

    Update

---
+++ EventHandler.jsx
@@ -0,0 +1,45 @@
+﻿/**
+ * It's not possible to use an event handler in a JSX file in the extensions folder that 
+ * can communicate with the javascript running in the CEP extension. As a workaround,
+ * the Adobe Bridge team recommends placing the JSX with event handler in the startup folder.
+ * Launch Bridge-> Go to Edit->Preferences -> Statup Scripts -> Reveal my Startup Scripts. 
+ * It opens the startup script folder. 
+ */
+
+
+if(xLib == undefined){
+  var xLib = new ExternalObject("lib:\PlugPlugExternalObject");
+}
+
+if(xmpLib == undefined){
+        if( Folder.fs == "Windows" ){
+            var pathToLib = Folder.startup.fsName + "/AdobeXMPScript.dll";
+        }else{
+            var pathToLib = Folder.startup.fsName + "/AdobeXMPScript.framework";
+        }
+        var libfile = new File( pathToLib );
+        var xmpLib = new ExternalObject("lib:" + pathToLib );
+}
+
+dispatchCepEvent = function (in_eventType, in_message) {
+   
+    if (xLib) {
+        var eventObj = new CSXSEvent();
+        eventObj.type = in_eventType;
+        eventObj.data = in_message;
+        eventObj.dispatch();
+    }
+}
+
+onSelectedThumb = function(event){
+   
+    if(event.object instanceof Document && event.type == "selectionsChanged") {
+
+            dispatchCepEvent("cep.extendscript.event.mySelection", "");
+    }
+}
+
+
+app.eventHandlers.push({handler: onSelectedThumb});
+
+
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # AEM-Bridge2018-Sample-Extension
+It's not possible to use an event handler in a JSX file in the extensions folder that can communicate with the javascript running in the CEP extension. As a workaround,
+the Adobe Bridge team recommends placing the JSX with event handler in the startup folder. (i.e. C:\Users\USERNAME\AppData\Roaming\Adobe\Bridge CC 2018\Startup Scripts)
+
+Put this event handler call in your main.js file to call function.
+
+ //Begin event handler call from JSX to trigger populate    
+    csInterface.addEventListener("cep.extendscript.event.mySelection", function() {
+    csInterface.evalScript( 'sendObjToHTML()', function(result) {       
+                var data= result;
+                populate('#metadata', $.parseJSON(data)); 
+            });
+        });    
+ //End event handler call from JSX to trigger populate
